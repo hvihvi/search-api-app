26cb0453a47d53e1b441acc58bd46d5ad62f33a7
hvi <hvi@lesfurets.com>
2021-09-18 14:13:28 +0200

    Place input in a bottom fixed container (easy thumb access)

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,26 @@ import React, { useState } from "react";
 import "./App.css";
 import { TextInput } from "./design-system/TextInput";
 import { useSearchResult } from "./useSearchResult";
+import { BottomContainer } from "./design-system/BottomContainer";
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const searchResult = useSearchResult(searchTerm);
   return (
     <div>
-      <TextInput
-        id="searchProduct"
-        onChange={(e) => setSearchTerm(e.target.value)}
-        value={searchTerm}
-        label="Rechercher un produit"
-      />
       {searchResult?.products
         .map((p) => p.image_front_url)
         .map((src) => (
           <img src={src} alt={"produit"} />
         ))}
+      <BottomContainer>
+        <TextInput
+          id="searchProduct"
+          onChange={(e) => setSearchTerm(e.target.value)}
+          value={searchTerm}
+          label="Rechercher un produit"
+        />
+      </BottomContainer>
     </div>
   );
 };
---
+++ src/design-system/BottomContainer.tsx
@@ -0,0 +1,6 @@
+import { FC } from "react";
+import "./BottomContainer.css";
+
+export const BottomContainer: FC = ({ children }) => {
+  return <section className="BottomContainer">{children}</section>;
+};
