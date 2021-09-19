7c9b43a250e1b145f02cb1d3ebccd78147fb9ef0
hvi <hvi@lesfurets.com>
2021-09-19 23:43:18 +0200

    Add tests for initial state and a product search scenario

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,36 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import App from "./App";
+import mock from "./mock.json";
 
-// TODO mock fetch, add more test
-it("should render a text input", () => {
-  render(<App />);
-  const input = screen.getByLabelText(/Rechercher un produit/i);
-  expect(input).toBeInTheDocument();
+describe("My app", () => {
+  it("should render a text input", () => {
+    render(<App />);
+    const input = screen.getByLabelText(/Rechercher un produit/i);
+    expect(input).toBeInTheDocument();
+  });
+
+  it("should render a message when no search has been typed", () => {
+    render(<App />);
+    const input = screen.getByText(
+      /Vous pouvez rechercher un produit ci-dessous 🙂/i
+    );
+    expect(input).toBeInTheDocument();
+  });
+
+  it("should render images when typing IPA in the input", async () => {
+    // given
+    jest.mock("./http", () => ({
+      fetchOpenFoodFacts: jest.fn().mockReturnValue(Promise.resolve(mock)),
+    }));
+    render(<App />);
+    const input = screen.getByLabelText(/Rechercher un produit/i);
+
+    // when
+    fireEvent.change(input, { target: { value: "IPA" } });
+
+    // then
+    const brewdog = await screen.findByAltText("Punk IPA");
+    await waitFor(() => expect(brewdog).toBeInTheDocument());
+  });
 });
