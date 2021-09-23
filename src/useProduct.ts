c257ba476def6e018880cf12490e500572d637e8
hvi <hvi@lesfurets.com>
2021-09-23 12:30:56 +0200

    Remove comment

--- src/useProduct.ts
+++ src/useProduct.ts
@@ -4,7 +4,6 @@ import { fetchProduct, Product } from "./http";
 export const useProduct = (id: string) => {
   const [product, setProduct] = useState<Product>();
   useEffect(() => {
-    // delay/cleanup to not fetch when the user is still typing
     fetchProduct(id)
       .then((result) => setProduct(result.product))
       .catch((e) => console.log(e)); // TODO handle errors
