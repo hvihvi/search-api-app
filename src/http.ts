510bd2eee84c7a8bb9f07976c96c0a5ee349fa34
hvi <hvi@lesfurets.com>
2021-09-23 03:24:41 +0200

    Increase amount searched

--- src/http.ts
+++ src/http.ts
@@ -1,6 +1,6 @@
 export const fetchSearch = (searchTerm: string): Promise<SearchApiResponse> =>
   fetch(
-    `https://world.openfoodfacts.org/cgi/search.pl?search_terms=${searchTerm}&json=1&page_size=20&page=1`
+    `https://world.openfoodfacts.org/cgi/search.pl?search_terms=${searchTerm}&json=1&page_size=50&page=1`
   ).then((r) => r.json());
 
 export type SearchApiResponse = {
